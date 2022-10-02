2f76a3f1b9c46e8e2837d558f8bf663ba775069d
khanshorif331 <khanshorif331@gmail.com>
2022-10-02 20:29:13 +0600

    typescript setup done

---
+++ index.js
@@ -0,0 +1,6 @@
+var myAge = 27
+var ageCalc = function (age) {
+	return age * 2
+}
+console.log(ageCalc(myAge))
+console.log(ageCalc(54))
---
+++ index.ts
@@ -0,0 +1,6 @@
+let myAge:number = 27;
+
+let ageCalc = ( age: number) => {
+    return age * 2;
+}
+console.log(ageCalc(myAge));
