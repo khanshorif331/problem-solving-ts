88741a8f981a19d8347c92b415813753979db691
khanshorif331 <khanshorif331@gmail.com>
2022-10-02 20:51:22 +0600

    first problem soolved of adding two number

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var myAge = 27
+var myAge = 27;
 var ageCalc = function (age) {
-	return age * 2
-}
-console.log(ageCalc(myAge))
-console.log(ageCalc(54))
+    return age * 2;
+};
+// console.log(ageCalc(myAge));
+// Problem no.1
+// https://www.hackerrank.com/challenges/solve-me-first/problem
+var getSum = function (a, b) {
+    var sum = a + b;
+    return sum;
+};
+console.log(getSum(2, 3));
--- index.ts
+++ index.ts
@@ -3,4 +3,16 @@ let myAge:number = 27;
 let ageCalc = ( age: number) => {
     return age * 2;
 }
-console.log(ageCalc(myAge));
+// console.log(ageCalc(myAge));
+
+
+// Problem no.1
+// https://www.hackerrank.com/challenges/solve-me-first/problem
+
+
+const getSum = (a:number, b:number) => {
+    const sum:number = a + b;
+    return sum
+}
+console.log(getSum(2,3));
+
